bbbedd12dcf343f78031cc4fb8ea0e2d40affa80
Andreas Rauchenbacher <info@rauchenbacher.com>
2019-07-14 14:44:59 +0200

    Add possibilities to change the text and fontsize

--- leaflet-text.js
+++ leaflet-text.js
@@ -8,17 +8,44 @@
 (function () {
 
     L.TextIcon = L.DivIcon.extend({
+        initialize: function(options) {
+            if (!options.size) {
+                options.size = 24;
+            }
+            if (!options.color) {
+                options.color = 'blue';
+            }
+            if (!options.text) {
+                options.text = 'Text';
+            }
+            L.setOptions(this, options);
+        },
         createIcon: function() {
-            var textDiv = document.createElement('div');
-            textDiv.className = 'leaflet-marker-icon leaflet-text-marker';
+            var containerElement = document.createElement('div');
+            containerElement.className = 'leaflet-marker-icon leaflet-text-marker';
 
-            var span = document.createElement('span');
-            span.className = 'leaflet-color-' + (this.options.color ? this.options.color : 'blue');
-            span.style.fontSize = (this.options.size ? this.options.size : 24) + 'px';
-            span.innerText = this.options.text;
+            this._spanElement = document.createElement('span');
+            this.update();
 
-            textDiv.appendChild(span);
-            return textDiv;
+            containerElement.appendChild(this._spanElement);
+            return containerElement;
+        },
+        update: function() {
+            this._spanElement.className = 'leaflet-color-' + this.options.color ;
+            this._spanElement.style.fontSize = this.options.size + 'px';
+            this._spanElement.innerText = this.options.text;
+        },
+        fontsizeIncrease: function() {
+            this.options.size += 8;
+            this.update();
+        },
+        fontsizeDecrease: function() {
+            this.options.size -= 8;
+            this.update();
+        },
+        setText: function(text) {
+            this.options.text = text;
+            this.update();
         }
     });
 
